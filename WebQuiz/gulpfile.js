a2ec28faf98e94c268bdbe9debcaf61f28368863
Carlos Martinez <carlos@buildsoft.com.au>
2016-11-08 16:11:30 +1100

    Minor changes

--- WebQuiz/gulpfile.js
+++ WebQuiz/gulpfile.js
@@ -1,5 +1,4 @@
-/// <binding AfterBuild='Common:dev, Common:fonts' ProjectOpened='watch' />
-
+/// <binding BeforeBuild='css, js, fonts' />
 /*
 This file in the main entry point for defining Gulp tasks and using Gulp plugins.
 Click here to learn more. http://go.microsoft.com/fwlink/?LinkId=518007
